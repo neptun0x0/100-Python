fe661bf39897d99775406e008c74beb6ebf8bc50
neptun0x0 <66038317+neptun0x0@users.noreply.github.com>
2020-05-31 01:11:48 +0100

    Update kalkulator miar.py
    final

--- kalkulator miar.py
+++ kalkulator miar.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import math
+import sys
 
-def moc
+def moc():
     y = 0
     z = 0
     x = input("Podaj Moc ")
@@ -12,11 +13,11 @@ def moc
     y = 1.35962162 * x
     z = 0.73549875 * x
 
-    print(round(y))
+
     print(str(x) + " kW = "+ str(round(y,2)) + " KM")
     print(str(x) + " KM = "+ str(round(z,2)) + " kW")
 
-def energia
+def energia():
     y = 0
     z = 0
     x = input("Podaj Energię ")
@@ -27,11 +28,10 @@ def energia
     y = x / 3600000
     z = 3600000 * x
 
-    print(round(y))
     print(str(x) + " J = "+ str(y) + " kWh")
     print(str(x) + " kWh = "+ str(round(z,2)) + " J")
 
-def masa
+def masa():
     y = 0
     z = 0
     x = input("Podaj Masę ")
@@ -42,11 +42,10 @@ def masa
     y = 0.0022046 * x
     z = 453.59237 * x
 
-    print(round(y))
     print(str(x) + " g = "+ str(round(y,6)) + " lbs")
     print(str(x) + " lbs = "+ str(round(z,2)) + " g")
 
-def temp
+def temp():
     y = 0
     z = 0
     x = input("Podaj Temperaturę ")
@@ -60,3 +59,28 @@ def temp
 
     print(str(x) + " C = "+ str(y) + " K")
     print(str(x) + " K = "+ str(round(z,2)) + " C")
+
+
+
+print("Wciśnij 1 aby obliczyć Moc")
+print("Wciśnij 2 aby obliczyć Energię")
+print("Wciśnij 3 aby obliczyć Masę")
+print("Wciśnij 4 aby obliczyć Temperaturę")
+print("Wciśnij 5 aby wyjść ")
+i = int(input())
+
+j = 2
+if i == 1:
+    moc()
+elif i == 2:
+    energia()
+elif i == 3:
+    masa()
+elif i == 4:
+    temp()                    
+elif i == 5:
+    print("Bye! Bye! Bye! Bye! Bye! Bye! Bye! Bye!")
+    sys.exit()
+else:
+    print("Nie umiesz nawet wpisać poprawnej liczby idioto?")
+    print("Bye Bye!")
